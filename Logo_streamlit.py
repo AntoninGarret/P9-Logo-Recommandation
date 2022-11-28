0e1bcf5092780a6c3ec9d978ab2361cd4835d6f0
unknown <antonin.garret@ens-rennes.fr>
2022-11-28 13:36:02 +0100

    Added streamlit application

---
+++ Logo_streamlit.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import requests
+
+st.title("MVP : Recommandation d'article pour la bibliothèque LOGO")
+
+
+uid = st.text_input("Entrez votre identifiant utilisateur")
+
+headers = {"Content-Type": "application/json"}
+
+url = "https://logo-recommandation.azurewebsites.net/api/user/" + uid
+#st.write("POST to url", uri)
+
+try:
+    resp = requests.post(url)
+except:
+    st.error("Failed to connect to the API")
+if resp.text is not "":
+    st.write("Articles recommandés : ", resp.text)
+
